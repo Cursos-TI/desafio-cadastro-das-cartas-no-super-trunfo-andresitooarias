d94406360d30ec710e9324d30ff944539152cc96
programmerrooki <76979779+andresitooarias@users.noreply.github.com>
2025-04-02 22:48:07 +0000

    Foram feitas algumas modificações no código para melhorar a legibilidade e a eficiência. As principais mudanças incluem: - Remoção de variáveis desnecessárias. - Melhoria na formatação do código. - Adição de comentários para melhor compreensão do código.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,45 +19,43 @@ int main()
 
     printf("Iremos começar com as informações da sua primeira carta, ok? Vamos nessa!\n");
 
-    printf("Para que possamos começar por favor insira uma letra de A - H para seu Estado: ");
+    printf("Para que possamos começar por favor insira uma letra de A - H para seu Estado:\n");
     scanf("%c", &estado);
-    printf("A letra inserida foi: %c\n\n", estado);
+   // printf("A letra inserida foi: %c\n\n", estado);
 
-    printf("Em seguida preciso que você insira a letra do Estado seguido de um número de 01 a 04, (ex: A01, B03). Este será o código da sua carta: ");
+    printf("Em seguida preciso que você insira a letra do Estado seguido de um número de 01 a 04, (ex: A01, B03). Este será o código da sua carta:\n");
     scanf("%s", codigoCarta);
-    printf("O código da carta inserido foi: %s\n\n", codigoCarta);
+   // printf("O código da carta inserido foi: %s\n\n", codigoCarta);
 
-    printf("Insira o nome da sua Cidade: ");
+    printf("Insira o nome da sua Cidade:\n");
     scanf("%s", nomeCidade);
-    printf("O nome da cidade é: %s\n\n", nomeCidade);
+  //  printf("O nome da cidade é: %s\n\n", nomeCidade);
 
-    printf("Insira o número de habitantes da sua cidade: ");
+    printf("Insira o número de habitantes da sua cidade:\n");
     scanf("%d", &populacao);
-    printf("O número de habitantes da sua cidade é: %d\n\n", populacao);
+   // printf("O número de habitantes da sua cidade é: %d\n\n", populacao);
 
-    printf("Insira a área da sua cidade em (km²): ");
+    printf("Insira a área da sua cidade em (km²):\n");
     scanf("%f", &area);
-    printf("O número de habitantes da sua cidade é: %.2fkm²\n\n", area);
+    //printf("O número de habitantes da sua cidade é: %.2fkm²\n\n", area);
 
-    printf("Insira o pib da sua cidade: ");
+    printf("Insira o pib da sua cidade:\n");
     scanf("%f", &pib);
-    printf("O PIB digitado foi: %.2f bilhões de reais\n\n", pib);
+    //printf("O PIB digitado foi: %.2f bilhões de reais\n\n", pib);
 
-    printf("Insira a quantidade de pontos turísticos que a sua Cidade possui: ");
+    printf("Insira a quantidade de pontos turísticos que a sua Cidade possui:\n");
     scanf("%d", &numeroPontosTuristicos);
-    printf("O número de pontos turísticos da sua cidade é: %d\n\n", numeroPontosTuristicos);
+    //printf("O número de pontos turísticos da sua cidade é: %d\n\n", numeroPontosTuristicos);
 
     printf("Muito bem! Você acabou de inserir os dados da sua primeira carta, eles estarão sendo mostrados abaixo:\n\n");
 
-    printf("Estado: %c\n", estado);
-    printf("Código: %s\n", codigoCarta);
-    printf("Nome da cidade: %s\n",nomeCidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %f\n", area);
-    printf("PIB: %f\n", pib);
-    printf("Número de pontos turísticos: %d\n", numeroPontosTuristicos);
-
-
+    printf("A letra inserida foi: %c\n", estado);
+    printf("O código da carta inserido foi: %s\n", codigoCarta);
+    printf("O nome da cidade é: %s\n", nomeCidade);
+    printf("O número de habitantes da sua cidade é: %d\n", populacao);
+    printf("O número de habitantes da sua cidade é: %.2fkm²\n", area);
+    printf("O PIB digitado foi: %.2f bilhões de reais\n", pib);
+    printf("O número de pontos turísticos da sua cidade é: %d\n", numeroPontosTuristicos);
 
     return 0;
 }
